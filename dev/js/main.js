1eec6ff294b2734ad48e166e44d30a458971f5ef
Cristian Quiroz <cquirozp@alumnos.upacifico.cl>
2018-02-27 17:53:06 -0300

    Implementa correciones solicitadas por cliente

--- dev/js/main.js
+++ dev/js/main.js
@@ -1,4 +1,18 @@
 $(document).ready(function() {
-	console.log('this is test');
+	$(window).scroll(function(){
+		var winScroll = $(window).scrollTop();
+		if(winScroll > 51) {
+			$('.c-header__main').addClass('nav__fixed');
+		}else{
+			$('.c-header__main').removeClass('nav__fixed');
+		}
+	});
+
+	$('a[href="/#!/goForm"]').click(function(e){
+		$("html, body").animate({
+			scrollTop: $('.c-contacto').offset().top
+		}, 2000);		
+		e.preventDefault();
+	});
 });
 
