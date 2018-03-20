e5fad5ea8f7a647f632765603eeeacc1f77ef780
Cristian Quiroz <cquirozp@alumnos.upacifico.cl>
2018-03-20 15:02:15 -0300

    Implementa correciones y carrusel

--- dev/js/main.js
+++ dev/js/main.js
@@ -16,5 +16,98 @@ $(document).ready(function() {
 		}, 2000);		
 		e.preventDefault();
 	});
+
+	$('a[href="/!#/detalle"]').click(function(e){
+		$('.c-ofertas__info').toggleClass('c-ofertas__info--tall');
+		e.preventDefault();
+	});
+
+	$('.firstProduct').viewportChecker({
+		classToAdd: 'visible',
+		classToAddForFullView: 'full-visible',
+		classToRemove: 'invisible',
+		removeClassAfterAnimation: false,
+		offset: 200,
+		invertBottomOffset: true,
+		repeat: false,
+		callbackFunction: function (elem, action) {
+			setTimeout(function(){
+				$('.firstProduct').addClass('animated fadeIn');
+			},100)
+		},
+		scrollHorizontal: false
+	});	
+	
+	$('.firstSeparador').viewportChecker({
+		classToAdd: 'visible',
+		classToAddForFullView: 'full-visible',
+		classToRemove: 'invisible',
+		removeClassAfterAnimation: false,
+		offset: 200,
+		invertBottomOffset: true,
+		repeat: false,
+		callbackFunction: function (elem, action) {
+			setTimeout(function () {
+				$('.firstSeparador').addClass('animated fadeIn');
+			}, 100)
+		},
+		scrollHorizontal: false
+	});	
+
+	$('.secondProduct').viewportChecker({
+		classToAdd: 'visible',
+		classToAddForFullView: 'full-visible',
+		classToRemove: 'invisible',
+		removeClassAfterAnimation: false,
+		offset: 200,
+		invertBottomOffset: true,
+		repeat: false,
+		callbackFunction: function (elem, action) {
+			setTimeout(function(){
+				$('.secondProduct').addClass('animated fadeIn');
+			},100)
+		},
+		scrollHorizontal: false
+	});	
+	
+	$('.secondSeparador').viewportChecker({
+		classToAdd: 'visible',
+		classToAddForFullView: 'full-visible',
+		classToRemove: 'invisible',
+		removeClassAfterAnimation: false,
+		offset: 200,
+		invertBottomOffset: true,
+		repeat: false,
+		callbackFunction: function (elem, action) {
+			setTimeout(function () {
+				$('.secondSeparador').addClass('animated fadeIn');
+			}, 100)
+		},
+		scrollHorizontal: false
+	});
+		
+	$('.thirdProduct').viewportChecker({
+		classToAdd: 'visible',
+		classToAddForFullView: 'full-visible',
+		classToRemove: 'invisible',
+		removeClassAfterAnimation: false,
+		offset: 200,
+		invertBottomOffset: true,
+		repeat: false,
+		callbackFunction: function (elem, action) {
+			setTimeout(function(){
+				$('.thirdProduct').addClass('animated fadeIn');
+			},100)
+		},
+		scrollHorizontal: false
+	});	
+	
+	$('.owl-carousel').owlCarousel({
+		loop: true,
+		margin: 0,
+		nav: false,
+		dots: true,
+		items: 1,
+	})	
 });
 
